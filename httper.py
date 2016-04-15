1a7c4dc66eaf445dd088351639c32950c109f4da
lijie <popexizhi@gmail.com>
2016-04-15 11:48:16 +0800

    add dev_license_url

--- httper.py
+++ httper.py
@@ -5,11 +5,22 @@ import json
 
 class httper():
     def __init__(self, http_ip = "192.168.1.43"):
-        self.add_licenses = "http://"+ http_ip +":18080/api/biz/licenses"
-        
+        self.http_ip = http_ip
+
+
     def add_appserver_lic(self, name):
+        self.add_licenses = "http://"+ self.http_ip +":18080/api/biz/licenses"
         data = json.dumps({"customer_name": name,"product_name":"app_server"})
-        req = urllib2.Request(self.add_licenses, data, {'Content-Type': 'application/json'}) #添加发送头
+        return self._send_data(self.add_licenses, data)
+    
+    def add_dev_lic(self, applications_name, email = "testdev@senlime.com"):
+        self.add_dev_licenses = "http://"+ self.http_ip +":18080/api/admin/pin"
+        data = json.dumps({"email": email,"applications":applications_name})
+        
+        return self._send_data(self.add_dev_licenses, data)
+
+    def _send_data(self, url, data):
+        req = urllib2.Request(url, data, {'Content-Type': 'application/json'}) #添加发送头
 
         f = urllib2.urlopen(req)
         get_data = f.read()
@@ -19,3 +30,5 @@ class httper():
 if __name__ == "__main__":
     a = httper()
     print a.add_appserver_lic("xd")
+    print a.add_dev_lic([2])
+
