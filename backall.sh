06064ab6bf8ea6fe64e30f88f3bab5e34494eebc
lijie <popexizhi@gmail.com>
2016-04-21 17:24:42 +0800

    打印结果显示位置

--- backall.sh
+++ backall.sh
@@ -3,3 +3,4 @@ echo `cp ue_provision_res_$1.html /data/provision_test/$1`
 echo `mv log /data/provision_test/$1`
 echo `cp -rf /corefile /data/provision_test/$1`
 echo `cd /corefile && rm -rf *`
+echo "http://192.168.1.25/$1/ue_provision_res_$1.html"
