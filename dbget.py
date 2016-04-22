957d2c9eab83d9d3fe974817e50ecb6c190b048d
lijie <popexizhi@gmail.com>
2016-04-22 16:07:29 +0800

    修改db地址

--- dbget.py
+++ dbget.py
@@ -1,7 +1,7 @@
 #-*-coding:utf8-*-
 import MySQLdb
 class db_mod():
-    def __init__(self, db_name = "nexus_eap_test" , ip = "192.168.1.196", user = "root", pd = "Admin123"):
+    def __init__(self, db_name = "nexus_eap_test" , ip = "192.168.1.44", user = "slim", pd = "password"):
         self.db = MySQLdb.connect(ip ,user, pd, db_name, port=3306, charset="utf8")
         self.cursor = self.db.cursor()        
 
@@ -19,4 +19,4 @@ class db_mod():
 
 if __name__ == "__main__":
     x = db_mod()
-    x.get_pin()
+    print x.get_pin()
