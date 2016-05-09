01a4df82cbb2750bdfd3f2f749c1d44ba8ed3214
lijie <popexizhi@gmail.com>
2016-05-09 15:41:51 +0800

    备份进程和appserver 的log

--- backall.sh
+++ backall.sh
@@ -1,7 +1,12 @@
 echo `mkdir /data/provision_test/$1`
 echo `cp ue_provision_res_$1.html /data/provision_test/$1`
+echo "%%%%%%%%%%%%%%%%%%%%%%%%cp%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
+echo `cp app_server /data/provision_test/$1`
+echo `cp slim_engine_test /data/provision_test/$1`
+echo `cp app_server.log.txt /data/provision_test/$1`
 echo `mv log /data/provision_test/$1`
 echo `cp -rf /corefile /data/provision_test/$1`
+
 echo `cd /corefile && rm -rf *`
 echo "http://192.168.1.25/$1/ue_provision_res_$1.html"
 
