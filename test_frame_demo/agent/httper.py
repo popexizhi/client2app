0fdfa45f613caecdb58096d7f5483638198eca5e
lijie <popexizhi@gmail.com>
2016-06-21 18:55:49 +0800

    添加mapping存储配置map_list

--- test_frame_demo/agent/httper.py
+++ test_frame_demo/agent/httper.py
@@ -1,8 +1,9 @@
 #-*-coding:utf8-*-
 import urllib2
+from mapping import *
 
 class httper():
-    def __init__(self, url_base = "http://192.168.1.25/testcase_use/"):
+    def __init__(self, url_base = environment_map["httper"]["url"]):
         self.url_b = url_base
 
     def get_xml(self, f_id):
@@ -13,6 +14,16 @@ class httper():
         
         return con_file
 
+    def get_bfile(self, f_name):
+        tc_filelist = environment_map["httper"]["file_list"]
+        tc_rul = self.url_b + tc_filelist + str(f_name)
+        f = open(f_name, "wb") 
+        con_file = urllib2.urlopen(tc_rul).read() # next 做404.502的异常处理
+        f.write(con_file)
+        f.close()
+        
 if __name__=="__main__":
     x = httper()
-    print x.get_xml("1466129033")
+    #print x.get_xml("1466129033")
+    f_name = "nplServer1465202670.db"
+    x.get_bfile(f_name)
---
+++ test_frame_demo/agent/mapping.py
@@ -0,0 +1,14 @@
+# -*- coding:utf8 -*-
+# map_list
+
+environment_map = {
+    # 依赖服务环境配置         
+    "httper" :{
+        "url": "http://192.168.1.25/testcase_use/",
+        "file_list": "file_list/",
+    },                
+                
+                
+                
+                
+    }
