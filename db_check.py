a754c7cca173483364830275bb20e7abf76967d9
popexizhi105 <popexizhi@gmail.com>
2016-07-05 19:18:11 +0800

    db_check is pass

--- db_check.py
+++ db_check.py
@@ -6,20 +6,66 @@ class db_check():
     def __init__(self):
         pass
 
-    def db_save(self, dir_p="back_db"):
-        pa = dir_p
-        
+    def db_save(self, dir_p=".//", t_pa = "back_db//"):
+        pa = t_pa
+        pre = "nplServer"
+        os.system("mkdir %s" % t_pa)
+        file_list = self.get_all_db(dir_p)
+        res = 0
+        for i in file_list:
+            self.log("now process db is %s" % i)
+            if re.findall(pre, i):
+                self.log("serverdb is %s"% str(i))
+                #serverdb process
+                res_db = self.save_app_db(i, t_pa)
+                self.log("target app db is %s" % str(res_db))
+                self.backdb(i, res_db)
+                res = res + 1
+            else:
+                #client process
+                res_db = self.save_ue_db(i, t_pa)
+                self.log("target ue db is %s" % str(res_db))
+                self.backdb(i, res_db)
+                res = res + 1
+
+        return res
+
+    def backdb(self, d_pa, t_pa):
+        cmd = "cp %s %s" % (str(d_pa), str(t_pa))
+        os.system(cmd)
+
+
+    def save_app_db(self, db_p, target_p):
+        sql = "SELECT host_id from AppServerHostInfo"
+        host_id = self.get_host_id(sql, db_p)
+        return "%snplServer%s.db" % (target_p, str(host_id))
+
+    def save_ue_db(self, db_p, target_p):
+        sql = "SELECT local_host_id from target_host_info ;"
+        host_id = self.get_host_id(sql, db_p)
+        return "%snpl%s.db" % (target_p, str(host_id))
+    
+    def get_host_id(self, sql, db_p):
+        con  = sqlite3.connect(db_p)
+        cursor = con.execute(sql)
+        for j in cursor:
+            self.log("host_id: %s" % str(j[0]))
+            res = j[0]
+        return res
+
     def get_all_db(self, pa="."):
         path = pa
-        self.log("pa = %s" % pa)
+        suf = "\.db$"
+        self.log("path = %s" % path)
         res = []
         x_dir = {}
         for i in os.listdir(path):
-            if re.findall("db$", i):
-                res.append(i) 
+            if re.findall(suf, i):
+                res.append(path+"//"+i) 
                 x_dir[i] = "0"
             else:
-                self.log(i)
+                #self.log(i)
+                pass
 
         return res
 
--- db_check_test.py
+++ db_check_test.py
@@ -8,16 +8,34 @@ import os
 class Test_db_check(unittest.TestCase):
 
     def test_db_save(self):
-        pa = "test_dir"
+        pa = "testcase//"
+        tar_pa = "testcaseback//"
+        os.system("rm -r %s" % tar_pa)
+
         x = db_check()
-        res = x.db_save(pa)
-        self.assertEqual(res,1)
+        res = x.db_save(pa, tar_pa)
+        self.assertEqual(res,12)
     def test_get_all_db(self):
         db_pa = "testcase"
         x = db_check()
         res = x.get_all_db(db_pa)
-        self.assertEqual(len(res), 6)
-        
+        self.assertEqual(len(res), 12)
+
+    def test_save_ue_db(self):    
+        db_path = "testcase//npl1792147212.db"
+        target_path = "testcase//"
+        pre_res = "npl19415.db"
+        x = db_check()
+        res = x.save_ue_db(db_path, target_path)
+        self.assertEqual(res, target_path+pre_res)
+
+    def test_save_app_db(self):    
+        db_path = "testcase//nplServer1467704905.db"
+        target_path = "testcase//"
+        pre_res = "nplServer19414.db"
+        x = db_check()
+        res = x.save_app_db(db_path, target_path)
+        self.assertEqual(res, target_path+pre_res)
 
 if __name__=="__main__":
     unittest.main()
