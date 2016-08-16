9313bd1aefa8e816c245e6cce13911e72c52ea17
popexizhi <popexizhi@gmail.com>
2016-08-16 17:13:34 +0800

    add httper_test.py

--- httper.py
+++ httper.py
@@ -7,37 +7,58 @@ class httper():
     def __init__(self, http_ip = "192.168.1.55", space_s = 0):
         self.http_ip = http_ip
         self.space_http_s = space_s #每个请求的间隔时间，防止服务器请求太过频繁
+        self.token = None
+
+    def log(self, message):
+        print "*" * 20
+        print "[httper] %s" % message
+
     def login(self, username="lijie@senlime.com", passwd="password"):
-        res=None
+        """post username+passwd api/eap/auth ,return token"""
+        self.add_licenses = "http://"+ self.http_ip +"/api/eap/auth"
+        data = json.dumps({"email":username, "password":passwd})
+        res = self._send_data(self.add_licenses, data)
+        assert 0 == res["result"]
+        self.token = res["data"]["token"]
         return res
 
     def register_app_server(self, url_id, name, key, serial):
-        "post id+key+serial /api/admin/register_app_server"
-        self.add_licenses = "http://"+ self.http_ip +":18080/api/admin/register_app_server"
-        data = json.dumps({"app_server_id":url_id, "customer_name":name, "license_key":key, "serial":serial})
-        return self._send_data(self.add_licenses, data)
+        """post id+key+serial /api/eap/appservers/<server_id>/activation"""
+        if None == self.token:
+            self.login() 
+        assert self.token #无token无法add
+        url_register = "/api/eap/appservers/%d/activation" % url_id
+        self.add_licenses = "http://"+ self.http_ip + url_register
+        value = {"app_server_id":url_id,"license":{"customer_name":name, "license_key":key, "serial":serial},"activation_type":"single","cluster_info":{"cluster_type":"join","custer_id":"","cluster_name":"","cluster_desc":""},"single_info":{"name":"xxx","description":"xxx"},"dbconfig":{"type":"mysql","hostname":"","port":"3306","username":"","password":""}}
+
+        data = json.dumps(value)
+        return self._send_data(self.add_licenses, data, self.token)
 
     def add_appserver_lic(self, name):
-        self.add_licenses = "http://"+ self.http_ip +":18080/api/biz/licenses"
+        self.add_licenses = "http://"+ self.http_ip +"/api/biz/licenses"
         data = json.dumps({"customer_name": name,"product_name":"app_server"})
         return self._send_data(self.add_licenses, data)
     
     def add_dev_lic(self, applications_name, email = "testdev@senlime.com"):
-        self.add_dev_licenses = "http://"+ self.http_ip +":18080/api/admin/pin"
+        self.add_dev_licenses = "http://"+ self.http_ip +"/api/admin/pin"
         data = json.dumps({"email": email,"applications":applications_name})
         
         return self._send_data(self.add_dev_licenses, data)
 
-    def _send_data(self, url, data):
-        print url
-        print data
-        req = urllib2.Request(url, data, {'Content-Type': 'application/json'}) #添加发送头
-        print "[httper] "+"*** " * 20
-        print "url is %s; data is %s" % (url, data) 
-        print "[httper] "+"*** " * 20
+    def _send_data(self, url, data, token=None):
+        self.log("url is %s" % url)
+        self.log("post data is %s" % str(data))
+        if None == token:
+            header = {'Content-Type': 'application/json'}
+        else:
+            header = {'Content-Type': 'application/json', 'Authorization': token}
+        self.log("header is %s" % str(header))
+        req = urllib2.Request(url, data, header) #添加发送头
+        
         f = urllib2.urlopen(req)
         get_data = f.read()
         f.close()
+        self.log(get_data)
         return json.loads(get_data)
 
     def add_devs(self, application_ids =[3], dev_num = 100, dev_start = 0):
--- httper_test.py
+++ httper_test.py
@@ -4,10 +4,29 @@ import time
 import unittest
 
 class TestHttper(unittest.TestCase):
-        def test_login(self):
-            x = httper()
-            cookie = x.login()
-            assert cookie
+    def test_login(self):
+        x = httper("192.168.1.43:18080")
+        res = x.login()
+        assert res
+
+    def test_register_app_server(self):
+        x = httper("192.168.1.43:18080")
+        x.login()
+        res = x.register_app_server(url_id=3, name="9", key="57d1a7b5-5aeb-9a3e-b5bc-b37321bc83e6", serial="5a9a9bf91bc6206f00ab1a67f72bb75fe70cd4d4003ea1f28aa3eaafc5339416")
+        print res
+        p_res = 0
+        self.assertEqual(type(p_res), type(res["result"]))
+
+    def test_add_appserver_lic(self):
+        x = httper("192.168.1.43:18080")
+        res = x.add_appserver_lic(str(time.time()))
+        pre_res = 0
+        self.assertEqual(pre_res, res["result"])
+        assert res["key"]
+        assert res["serial"]
+        
+        
+
 
 if __name__=="__main__":
     unittest.main()
