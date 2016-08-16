4f590b21f4bdbfdc5ad6257b4d2dcd7a7df514c5
lijie <popexizhi@gmail.com>
2016-08-16 14:52:02 +0800

    add appserver_c uinittest for get_url wait/timeout

--- appserver_c.py
+++ appserver_c.py
@@ -1,27 +1,60 @@
 #-*- coding:utf8 -*-
 import sys
-from shell_con import sh_control
-from pexpect_shell import sh_pex 
+from db_Driver import sqlite_Driver
 from httper import httper
 import thread
 import time
+class appserver_c():
+    def __init__(self, db_name="nplServer1.db", cfg="alone_app.cfg"):
+        self.db = sqlite_Driver(db_name)
+        self.cfg = cfg
+        self.wait_time = 3 * 60 #appserver provision 超时时间
+
+    def log(self, message):
+        print "~" * 20
+        print message
+
+    def start_provision(self):
+        """change cfg start appserver provision """
+        pass
+    def get_url(self):
+        wait_time = self.wait_time
+        assert self.db
+        server_id = self.db.get_server_id()
+        res = "/api/eap/appservers/%d/activation"
+        use_time = 0
+        while None == server_id and use_time < wait_time:
+            time.sleep(5)
+            use_time = use_time + 5
+            server_id = self.db.get_server_id()
+
+        if None == server_id:
+            res = "timeout err"
+            # wait timeout for server_id
+        else:
+            res = res % server_id
+
+        self.log(res)
+        return res        
+    
 def app_provision(num, app_Mon):
     """
-    1.start appserver
-    2.wait log 打印 request url id
-    3.post id+key+serial /api/admin/register_app_server
+    1.change app cfg start appserver
+    2.wait appdb server_id
+    3.post id+key+serial /api/eap/appservers/<server_id>/activation
     4.wait log 打印 Please Add User Pin...
     """
-    a = sh_pex()
-    #1.start appserver
-    #2.wait log 打印 request url id
+    appserver = appserver_c()
+    #1.change app cfg start appserver
+    appserver.start_provision()
+    #2.wait appdb server_id
     url_id = a.get_url(num)
     assert url_id #检查返回的id一定存在
     print "** " * 20
     print "get url_id is "
     print url_id
 
-    #3.post id+key+serial /api/admin/register_app_server
+    #3.post id+key+serial /api/eap/appservers/<server_id>/activation
     print "** " * 20
     print "3.post id+key+serial /api/admin/register_app_server"
     http_x = httper("192.168.1.42")
---
+++ appserver_c_test.py
@@ -0,0 +1,66 @@
+# -*- coding:utf8 -*-
+from appserver_c import appserver_c
+import time
+import unittest
+import sqlite3
+import threading
+
+class TestAppserver_C(unittest.TestCase):
+    def test_start_provision(self):
+        x = appserver_c()
+        res = x.start_provision()
+        p_d = 0
+        self.assertEqual(p_d, res)
+    def test_get_url(self):
+        x = appserver_c(db_name="testdata\\nplServer1.db")
+        res = x.get_url()
+        p_d = "/api/eap/appservers/3/activation"
+        self.assertEqual(p_d, res)
+
+    def test_wait_get_url(self):
+        db_name = "testdata\\nplServer1_wait.db"
+        x = appserver_c(db_name)        
+        p_d = "/api/eap/appservers/3/activation"
+        s1 = threading.Thread(target=self._chang_db, args=(db_name,)) 
+        s1.start()
+        time.sleep(3)
+        res = x.get_url()
+        self.assertEqual(p_d, res)
+    
+    def test_timeout_get_url(self):
+        db_name = "testdata\\nplServer1_timeout.db"
+        x = appserver_c(db_name)        
+        p_d = "timeout err"
+        s1 = threading.Thread(target=self._chang_db, args=(db_name, "no_insert",)) 
+        s1.start()
+        time.sleep(3)
+        res = x.get_url()
+        self.assertEqual(p_d, res)
+
+    def _chang_db(self, db_name, has_insert = "has_insert"):
+        conn = sqlite3.connect(db_name)
+        drop_sql = """
+            DROP TABLE IF EXISTS "table_provision_status";
+            """
+        create_sql = """
+            CREATE TABLE "table_provision_status" (
+            "server_id"  INTEGER NOT NULL,
+            "prov_status"  TEXT NOT NULL,
+            "token"  TEXT NOT NULL,
+            PRIMARY KEY ("server_id" ASC)
+            );
+        """
+        c = conn.cursor()
+        c.execute(drop_sql)
+        c.execute(create_sql)
+        conn.commit()
+        if "has_insert" == has_insert :
+            time.sleep(10)
+            #conn = sqlite3.connect(db_name)
+            insert_sql = """INSERT INTO "table_provision_status" ("server_id", "prov_status", "token") VALUES ('3', 'Waiting_active', '1');"""
+            #c = conn.cursor()
+            c.execute(insert_sql)
+            conn.commit()
+        conn.close()
+if __name__=="__main__":
+    unittest.main()
